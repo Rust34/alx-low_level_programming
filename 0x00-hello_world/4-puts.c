629fe444970061886a22b6606693f96c588b2ecf
Rust34 <mwalikoricky@gmail.com>
2022-03-10 17:46:20 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,8 @@
  *
  * retuen 0 (success)
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+     puts("\"Programming is like building a multilingual puzzle");
+     return (0);
 }

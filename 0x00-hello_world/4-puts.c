8c73450a7f020a58fd8a6ed4580a98ff6f96a4dd
Rust34 <mwalikoricky@gmail.com>
2022-03-10 18:54:38 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
- #include <stdio.h>
+#include <stdio.h>
 
- /**
-  * main - prints "programming is like building a multilingual puzzle"
-  * Return: Always 0 (Succes)
-  */
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

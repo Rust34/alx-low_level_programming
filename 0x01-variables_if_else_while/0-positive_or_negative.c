81bbfbe98862590ac506e1fdd003146bfe10e7f8
Rust34 <mwalikoricky@gmail.com>
2022-03-11 18:15:53 -0800

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,8 @@
 #include <time.h>
 #include <stdio.h>
 
-/** main - assigns a random number to the variable n each time it is executed
+/** main - assigns a random number to the variable n each time
+ * it's executed and prints it
  * Return: Always 0 (Success)
  */
 int main(void)

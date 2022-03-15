7fc9490d37b309858ce2d34176a241cc8ac449b8
Rust34 <mwalikoricky@gmail.com>
2022-03-15 14:12:49 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,6 @@
  * _isalpha - checks for alphabetic character
  * @c: the character to be checked
  * Return: 1 if c is a letter, 0 otherwise
- * Return: Always 0 (Success)
  */
 int _isalpha(int c)
 {

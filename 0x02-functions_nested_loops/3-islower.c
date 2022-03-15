88465c0bcce0cc099dbd20dd43dbbb89df8f0cb3
Rust34 <mwalikoricky@gmail.com>
2022-03-15 13:37:20 -0700

    islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,22 @@
 
 /**
  * _islower - checks for lowercase character
- * 'c': the character to be checked
+ * @c: the character to be checked
  * Returns 1 if c is lowercase,otherwise 0
+ * Return: Always 0 (Success)
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
+	{
 		return (1);
+	}
 	else
-		return (0);
+	{
+		return (0)
+	}
+
+	_putchar('\n');
+	return (0);
+	
 }
